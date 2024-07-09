d64c784bb0d7b6ca7f21fd979145af3c50865056
cenk-idris <114963355+cenk-idris@users.noreply.github.com>
2024-07-09 15:14:05 +0300

    Setup firebase

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ChessApp extends StatelessWidget {
+  ChessApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chess',
+      home: Placeholder(),
+    );
+  }
+}
